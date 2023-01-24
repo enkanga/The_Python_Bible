3f1dabd4617bf01442ed6d7be417446ea585ae3f
Evelyn Nkanga <enkanga@gmail.com>
2023-01-23 23:48:00 -0500

    added full python scripts

--- Section7_Datastructures/cinema.py
+++ Section7_Datastructures/cinema.py
@@ -1,1 +1,25 @@
-# testing successful git integration on pc
+films = {
+    'Finding Dory': [3, 5],
+    'Bourne': [18, 5],
+    'Tarzan': [15, 5],
+    'Ghost Busters': [12, 5]
+}
+
+while True:
+    choice = input("What film would you like to watch?: ").strip().title()
+
+    if choice in films:
+        age = int(input('How old are you?: ').strip())
+        
+        # Check user age
+        if age >= films[choice][0]:
+
+            # Check enough seats
+            if films[choice][1] > 0:
+             print('Enjoy the film!')
+             films[choice][1] -= 1
+        else:
+            print('You are too young to see that film')
+
+    else:
+        print("We don't have that film....")
